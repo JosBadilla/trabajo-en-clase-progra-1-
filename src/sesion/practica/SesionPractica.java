b5c3e8f743944df159baff1c71997b8910b254c0
JossBadilla <josephbadilla2022@gmail.com>
2023-06-15 12:38:40 -0600

    trabajo en clase

---
+++ src/sesion/practica/SesionPractica.java
@@ -0,0 +1,23 @@
+
+package sesion.practica;
+
+public class SesionPractica {
+
+    public static void main(String[] args) {
+      
+      int num1 = 10;
+      int num2 = 5;
+      int num3 = 8;
+      if (num1 > num2 && num1 > num3 ){
+         System.out.println("El mayor numero es: " + num1);
+      } else if (num2 > num1 && num2 > num3 ){
+         System.out.println("El mayor numero es: " + num2);
+      } else {
+         System.out.println("El mayor numero es: " + num3);
+      }
+    
+    }
+    
+}
+    
+    
